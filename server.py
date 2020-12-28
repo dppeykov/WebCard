7c9bf1f69a63843360832daf2947b38562a35689
Damyan Peykov <dppeykov@gmail.com>
2020-12-28 09:13:00 +0100

    Adding dynamic year

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    current_year = datetime.datetime.now().year
+    print(current_year)
+    return render_template('index.html', year=current_year)
 
 
 if __name__ == '__main__':
